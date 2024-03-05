3497039d6335c9e6f32ee6a1025a66d511110f65
intellectus-rojiwon <jiwon.ro@int2.us>
2024-03-05 10:20:00 +0900

    feat: test option 적용

--- src/test/index.ts
+++ src/test/index.ts
@@ -6,6 +6,12 @@ import { Configuration } from '@APP/infrastructure/config';
 import { Mocker } from './mocker';
 import { Test } from './runner';
 
+const getArg = (key: string): string | null => {
+    const key_index = process.argv.findIndex((val) => val === key);
+    if (key_index === -1 || key_index + 1 >= process.argv.length) return null;
+    return process.argv[key_index + 1]!;
+};
+
 void (async () => {
     console.time('test time');
     Mocker.run();
@@ -14,8 +20,13 @@ void (async () => {
     const connection: IConnection = {
         host: `http://localhost:${Configuration.PORT}`,
     };
-
-    const result = await Test.run({ connection });
+    const skip = getArg('--skip');
+    const only = getArg('--only');
+    const result = await Test.run({
+        connection,
+        ...(skip ? { skip } : {}),
+        ...(only ? { only } : {}),
+    });
 
     await app.close();
     console.timeEnd('test time');
