942fb3de328517b3bdb83af86c1ff006a7d30438
intellectus-rojiwon <jiwon.ro@int2.us>
2024-03-07 18:02:32 +0900

    feat: add sleep api

--- src/app/syetem.controller.ts
+++ src/app/syetem.controller.ts
@@ -1,6 +1,11 @@
 import core from '@nestia/core';
 import * as nest from '@nestjs/common';
 
+function* waitSync(now: Date, sec: number) {
+    const expired_at = new Date(now.getTime() + sec * 1000);
+    while (new Date() < expired_at) yield true;
+}
+
 @nest.Controller('health')
 export class SystemController {
     /**
@@ -13,5 +18,12 @@ export class SystemController {
     @core.TypedRoute.Get()
     async check(): Promise<'hello world'> {
         return 'hello world';
+    }
+
+    @core.TypedRoute.Get('sleep')
+    sleep() {
+        const now = new Date();
+        for (const _ of waitSync(now, 60)) _;
+        return `${now.toISOString()} -> ${new Date().toISOString()}`;
     }
 }
